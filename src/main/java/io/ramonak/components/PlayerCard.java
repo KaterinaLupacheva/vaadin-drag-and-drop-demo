7c1a51f7bf3e3373934c71feaef5cd31b4dd8c5a
Katerina <katerinalupacheva@gmail.com>
2020-01-11 11:33:17 +0300

    Player Card set to draggable

--- src/main/java/io/ramonak/components/PlayerCard.java
+++ src/main/java/io/ramonak/components/PlayerCard.java
@@ -1,12 +1,13 @@
 package io.ramonak.components;
 
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.dnd.DragSource;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import io.ramonak.data.Player;
 
 @CssImport("./styles/player-card.css")
-public class PlayerCard extends HorizontalLayout {
+public class PlayerCard extends HorizontalLayout implements DragSource<PlayerCard> {
 
     private Div name = new Div();
     private Div number = new Div();
@@ -18,5 +19,7 @@ public class PlayerCard extends HorizontalLayout {
         number.setText(player.getNumber().toString());
         position.setText(player.getPosition().toString());
         add(number, position, name);
+        setDraggable(true);
+        setDragData(number.getText());
     }
 }
