e44a3520d7c4c14ec1c67aabbe1f213c72ac4b60
Katerina <katerinalupacheva@gmail.com>
2020-01-12 16:28:19 +0300

    Player Card refactored

--- src/main/java/io/ramonak/components/PlayerCard.java
+++ src/main/java/io/ramonak/components/PlayerCard.java
@@ -9,14 +9,13 @@ import io.ramonak.data.Player;
 @CssImport("./styles/player-card.css")
 public class PlayerCard extends HorizontalLayout implements DragSource<PlayerCard> {
 
-    private Div name = new Div();
-    private Div number = new Div();
-    private Div position = new Div();
-
     public PlayerCard(Player player) {
         addClassName("player-card");
+        Div name = new Div();
         name.setText(player.getName());
+        Div number = new Div();
         number.setText(player.getNumber().toString());
+        Div position = new Div();
         position.setText(player.getPosition().toString());
         add(number, position, name);
         setDraggable(true);
